736ae628197e6f58c71b0e3e3582d67289195ab8
ehdrnjs9022 <ehdrnjs9022@naver.com>
2025-06-27 17:30:31 +0900

    250627

--- src/main/java/com/kh/finalproject/auth/model/dto/LoginDTO.java
+++ src/main/java/com/kh/finalproject/auth/model/dto/LoginDTO.java
@@ -1,5 +1,8 @@
 package com.kh.finalproject.auth.model.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +15,12 @@ import lombok.Setter;
 
 public class LoginDTO {
 
-	
+	@Size(min = 8, max = 20, message = "아이디 길이는 8~20자 사이여야 합니다.")
+	@Pattern(regexp = "^[a-z][a-z0-9]{6,18}[0-9]$", message = "잘못된 아이디 형식입니다.")
+	@NotBlank(message = "아이디는 비어있거나 공백이 포함될 수 없습니다.") 
 	private String userId;
+	@Size(min = 8, max = 30, message = "비밀번호 길이는 8~30자 사이여야 합니다.")
+	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]).\\S{8,30}$", message = "잘못된 비밀번호 형식입니다.")
+	@NotBlank(message = "비밀번호는 비어있거나 공백이 포함될 수 없습니다.")
 	private String password;
 }
