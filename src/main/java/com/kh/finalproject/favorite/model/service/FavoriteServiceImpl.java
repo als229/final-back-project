e9a1cf92c8d1a3f7bb49bb831ad5ec94f4c4f1b1
LEKM <rhksalsdldia@naver.com>
2025-07-03 11:55:29 +0900

    re commit

---
+++ src/main/java/com/kh/finalproject/favorite/model/service/FavoriteServiceImpl.java
@@ -0,0 +1,12 @@
+package com.kh.finalproject.favorite.model.service;
+
+import com.kh.finalproject.favorite.model.dto.FavoriteDTO;
+
+public class FavoriteServiceImpl implements FavoriteService{
+
+	@Override
+	public void insertFavorite(FavoriteDTO favorite) {
+		
+	}
+
+}
